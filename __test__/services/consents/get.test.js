66beb76f0c8173bcdaf4807b181f060543ddfd95
Adam Näslund <adam.naslund@iteam.se>
2019-02-26 18:30:06 +0100

    Add route to operator for getting consent between account and client

--- __test__/services/consents/get.test.js
+++ __test__/services/consents/get.test.js
@@ -1,13 +1,21 @@
 const { get } = require('../../../lib/services/consents')
-const redis = require('../../../lib/adapters/redis')
 const pg = require('../../../__mocks__/pg')
 jest.mock('../../../lib/adapters/redis')
 
 describe('services/consents #get', () => {
   beforeEach(() => {
-    redis.set.mockResolvedValue('OK')
     pg.client.query.mockResolvedValue({
-      rows: [{}]
+      rows: [
+        {
+          consent_id: 'b2f5659f-6391-4068-9366-ae4803bd0a20',
+          client_id: 'https://someservice.tld',
+          processed: new Date('1551196280802')
+        }, {
+          consent_id: 'f4d16582-0008-4db3-86c4-29ea15e91038',
+          client_id: 'https://someservice.tld',
+          processed: new Date('1443333288888')
+        }
+      ]
     })
   })
   afterEach(() => {
@@ -16,7 +24,8 @@ describe('services/consents #get', () => {
   })
 
   it('queries the db', async () => {
-    await get('consent-id')
-    expect(pg.client.query).toHaveBeenCalled()
+    await get('account-id', 'client-id')
+    expect(pg.client.query).toHaveBeenCalledTimes(1)
+    expect(pg.client.query).toHaveBeenCalledWith(expect.anything(), ['account-id', 'client-id'])
   })
 })
